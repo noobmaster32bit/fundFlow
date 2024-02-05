8661902a33d827bfa6b0b3268023abb715d8ddd3
akshay <akshayajikumar10@gmail.com>
2024-02-05 15:49:41 +0530

    random

--- budget/views.py
+++ budget/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from django.views.generic import View
 from budget.models import Transaction
 from django import forms
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
 class TransactionForm(forms.ModelForm):
     class Meta:
         model=Transaction
@@ -10,7 +12,16 @@ class TransactionForm(forms.ModelForm):
         # fields = "__all__"      // all fields
         # fields = ["field1","field2",]
 
-  
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=["username","email","password"]
+
+class LoginForm(forms.Form):
+    username=forms.CharField()
+    password=forms.CharField()
+
+
 
 # View for listing all transactions
 # url localhost:8000/transactions/all/
@@ -44,4 +55,82 @@ class TransactionDetailView(View):
         id=kwargs.get("pk")
         qs=Transaction.objects.get(id=id)
         return render(request,"transaction_detail.html",{"data":qs})
+
+# View for transaction delete
+# url localhost:8000/transactions/{id}/remove/
+#  method : get   
+class TransactionDeleteView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        Transaction.objects.filter(id=id).delete()
+        return redirect("transaction-list")
+
+# View for transaction update
+# url localhost:8000/transactions/{id}/change/
+#  method : get,post      
+
+class TransactionUpdateView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        transaction_object=Transaction.objects.get(id=id)
+        form=TransactionForm(instance=transaction_object)
+        return render(request,"transaction_edit.html",{"form":form})
+    def post(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        transaction_object=Transaction.objects.get(id=id)
+        form=TransactionForm(request.POST,instance=transaction_object)
+        if form.is_valid():
+            form.save()
+            return redirect("transaction-list")
+        else:
+            return render(request,"transaction_edit.html",{"form":form})
+        
+# Signup view
+# url : localhost:8000/signup/
+# method : get,post 
+class SignUpView(View):
+    def get(self,request,*args,**kwargs):
+        form=RegistrationForm()
+        return render(request,"register.html",{"form":form})
+    def post(self,request,*args,**kwargs):
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(**form.cleaned_data)
+            print("user created successfully!")
+            return redirect("signin")
+        else:
+            print("User not created")
+            return render(request,"register.html",{"form":form})
+
+
+# SignIn view
+# url : localhost:8000/signin/
+# method : get,post 
+
+class SignInView(View):
+    def get(self,request,*args,**kwargs):
+        form=LoginForm()
+        return render(request,"login.html",{"form":form})
+    def post(self,request,*args,**kwargs):
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            u_name=form.cleaned_data.get("username")
+            pwd=form.cleaned_data.get("password")
+            user_object=authenticate(request,username=u_name,password=pwd)
+            if user_object:
+                login(request,user_object)
+                # request.user => current user
+                print("valid credentials")
+                return redirect("transaction-list")
+        print("invalid credentials")
+        return render(request,"login.html",{"form":form})
+        
+# SignOut view
+# url : localhost:8000/signin/
+# method : get
     
+class SignOutView(View):
+    def get(self,request,*args,**kwargs):
+        logout(request)
+        return redirect("signin")
+
--- fundflowapplication/urls.py
+++ fundflowapplication/urls.py
@@ -23,5 +23,9 @@ urlpatterns = [
     path('transactions/all/',views.TransactionListView.as_view(),name="transaction-list"),
     path('transactions/add/',views.TransactionCreateView.as_view(),name="transaction-add"),
     path('transactions/<int:pk>/',views.TransactionDetailView.as_view(),name="transaction-detail"),
-    
+    path('transactions/<int:pk>/remove',views.TransactionDeleteView.as_view(),name="transaction-delete"),
+    path('transactions/<int:pk>/change',views.TransactionUpdateView.as_view(),name="transaction-edit"),
+    path('signup/',views.SignUpView.as_view(),name='signup'),
+    path('',views.SignInView.as_view(),name="signin"),
+    path('signout/',views.SignOutView.as_view(),name="signout"),
 ]
